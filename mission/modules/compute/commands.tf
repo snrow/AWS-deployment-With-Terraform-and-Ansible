3bb876c5009a0271e545bff9ee026d6a6ef60984
snrow <eliranbenmaor@gmail.com>
2023-05-07 03:34:16 +0300

    fixed command file

--- mission/modules/compute/commands.tf
+++ mission/modules/compute/commands.tf
@@ -7,31 +7,31 @@ resource "null_resource" "IP_configure" {
 
             cat > /home/ubuntu/task/mission/ansible/inventory.yml <<EOF
             Accounting:
-            children:
-                db_servers:
-                hosts:
-                    DB:
-                    ansible_host: ${aws_instance.db.public_ip}
-                lb_servers:
-                hosts:
-                    LB:
-                    ansible_host: ${aws_instance.lb.public_ip}
-                web_servers:
-                hosts:
-                    WEB:
-                    ansible_host: ${aws_instance.web.public_ip}
-            vars:
-                ansible_ssh_private_key_file: /home/ubuntu/task/mission/eliran-task-key.pem
-                ansible_ssh_common_args: -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null
-                ansible_user: ubuntu
-                app_name: Test
-                db_port: 27017
-                db_servers:
-                - 192.168.0.13
-                lb_port: 80
-                web_server_port: 8080
-                web_servers:
-                - 192.168.0.12
+                children:
+                    db_servers:
+                    hosts:
+                        DB:
+                        ansible_host: ${aws_instance.db.public_ip}
+                    lb_servers:
+                    hosts:
+                        LB:
+                        ansible_host: ${aws_instance.lb.public_ip}
+                    web_servers:
+                    hosts:
+                        WEB:
+                        ansible_host: ${aws_instance.web.public_ip}
+                vars:
+                    ansible_ssh_private_key_file: /home/ubuntu/task/mission/eliran-task-key.pem
+                    ansible_ssh_common_args: -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null
+                    ansible_user: ubuntu
+                    app_name: Test
+                    db_port: 27017
+                    db_servers:
+                    - 192.168.0.13
+                    lb_port: 80
+                    web_server_port: 8080
+                    web_servers:
+                    - 192.168.0.12
             EOF
         EOT
     
